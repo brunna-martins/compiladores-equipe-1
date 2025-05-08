10f37019bccd106478afa2dbbbe64e21e770823b
Marianannn <mariana.leticia05@gmail.com>
2025-05-07 21:44:33 -0300

    test: adicionando testes para as funções

--- testes/teste-funcao.py
+++ testes/teste-funcao.py
@@ -1,2 +1,11 @@
-def funcao(x, y, z);
-def outra_funcao();
+def funcao(x, y, z):
+    x = 1
+    y = 3
+    a  = z + x + y
+    print(a)
+    return a
+
+funcao( 1, 1, 1)
+
+def outra_funcao():
+    return 0

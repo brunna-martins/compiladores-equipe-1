3a83b2476b9faff38e897ddf0df8eea9c0870385
TaynaraCris <taynaracris11@gmail.com>
2025-05-27 22:03:42 -0300

    #26 implementando estrutura da tabela e criando função inserir, buscar e remover simbolo

---
+++ tabela_simbolos.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tabela_simbolos.h"
+
+unsigned int hash(const char* chave) {
+    unsigned int h = 0;
+    for (; *chave; chave++)
+        h = (h << 4) + *chave;
+    return h % TAM_TABELA;
+}
+
+TabelaSimbolos* criar_tabela() {
+    TabelaSimbolos* nova = (TabelaSimbolos*)malloc(sizeof(TabelaSimbolos));
+    for (int i = 0; i < TAM_TABELA; i++)
+        nova->tabela[i] = NULL;
+    nova->anterior = NULL;
+    return nova;
+}
+
+void destruir_tabela(TabelaSimbolos* tabela) {
+    for (int i = 0; i < TAM_TABELA; i++) {
+        Simbolo* atual = tabela->tabela[i];
+        while (atual) {
+            Simbolo* temp = atual;
+            atual = atual->proximo;
+            free(temp->nome);
+            free(temp->tipo);
+            free(temp);
+        }
+    }
+    free(tabela);
+}
+
+int inserir_simbolo(TabelaSimbolos* tabela, const char* nome, const char* tipo) {
+    unsigned int indice = hash(nome);
+    Simbolo* novo = (Simbolo*)malloc(sizeof(Simbolo));
+    novo->nome = strdup(nome);
+    novo->tipo = strdup(tipo);
+    novo->proximo = tabela->tabela[indice];
+    tabela->tabela[indice] = novo;
+    return 1;
+}
+
+Simbolo* buscar_simbolo(TabelaSimbolos* tabela, const char* nome) {
+    for (TabelaSimbolos* atual = tabela; atual != NULL; atual = atual->anterior) {
+        unsigned int indice = hash(nome);
+        Simbolo* s = atual->tabela[indice];
+        while (s != NULL) {
+            if (strcmp(s->nome, nome) == 0)
+                return s;
+            s = s->proximo;
+        }
+    }
+    return NULL;
+}
+
+int remover_simbolo(TabelaSimbolos* tabela, const char* nome) {
+    unsigned int indice = hash(nome);
+    Simbolo* atual = tabela->tabela[indice];
+    Simbolo* anterior = NULL;
+    while (atual != NULL) {
+        if (strcmp(atual->nome, nome) == 0) {
+            if (anterior == NULL)
+                tabela->tabela[indice] = atual->proximo;
+            else
+                anterior->proximo = atual->proximo;
+            free(atual->nome);
+            free(atual->tipo);
+            free(atual);
+            return 1;
+        }
+        anterior = atual;
+        atual = atual->proximo;
+    }
+    return 0;
+}
+
+TabelaSimbolos* empilhar_escopo(TabelaSimbolos* atual) {
+    TabelaSimbolos* novo = criar_tabela();
+    novo->anterior = atual;
+    return novo;
+}
+
+TabelaSimbolos* desempilhar_escopo(TabelaSimbolos* atual) {
+    TabelaSimbolos* anterior = atual->anterior;
+    destruir_tabela(atual);
+    return anterior;
+}
+
